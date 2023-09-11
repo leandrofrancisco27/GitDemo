8293cb96514e914d334dc89e465a7c079f76fdf6
leandro <leandro.francisco@globant.com>
2023-09-11 16:21:26 -0300

    Architect asian develop

--- src/test/java/stepDefinitions/hooks.java
+++ src/test/java/stepDefinitions/hooks.java
@@ -4,18 +4,18 @@ import io.cucumber.java.After;
 import io.cucumber.java.Before;
 
 public class hooks {
-
+	// coments
 	@Before("@NetBanking")
 	public void netBankingSetup() {
 		System.out.println("******************");
 		System.out.println("setup the entries in Netbanking database");
 	}
-	
+	// coments
 	@After
 	public void tearDown() {
 		System.out.println("clear the entries");
 	}
-	
+	// coments
 	@Before("@Mortgage")
 	public void mortgageSetup() {
 		
